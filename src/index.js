d5b7b4ad733599de7e0a7b81514b023a6fb0f4eb
Lukas Jakob <lu.jakob@googlemail.com>
2017-12-21 17:34:15 +0100

    Add morgan, cors, bodyparser

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import Express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
-import {graphqlExpress, graphiqlExpress} from 'graphql-server-express'
-import bodyParser from 'body-parser'
+import {graphqlExpress, graphiqlExpress} from 'graphql-server-express';
 
 import {schema} from './schema'
 
-let PORT = 3000;
-
-if (process.env.PORT) {
-  PORT = parseInt(process.env.PORT, 10);
-}
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+const isProduction = process.env.NODE_ENV === 'production';
 
 const app = new Express();
 
+app.use(cors());
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
